fa3ff9c6c112d4a0eba1c36ae6ce2cc6b87d6ce3
r.memetov <r.memetov@qiwi.com>
2020-02-12 10:14:27 +0300

    Метод рассчета расстояния между точками

---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,18 @@
+public class Point {
+    public static void main(String[] args){
+
+        double x1 = 3;
+        double y1 = 1;
+        double x2 = 2;
+        double y2 = 0;
+
+
+
+        System.out.println("Расстояние между точками " + " = " + distance(x1, y1, x2, y2));
+    }
+
+
+    public static double distance(double x1, double y1, double x2, double y2){
+        return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) *  (x2 - x1));
+    }
+}
