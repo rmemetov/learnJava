d69ffa418cf6c2440885e7e7b96de4fac87fa83b
r.memetov <r.memetov@qiwi.com>
2020-02-12 10:16:28 +0300

    formatting

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,5 +1,5 @@
 public class Point {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         double x1 = 3;
         double y1 = 1;
@@ -7,12 +7,11 @@ public class Point {
         double y2 = 0;
 
 
-
         System.out.println("Расстояние между точками " + " = " + distance(x1, y1, x2, y2));
     }
 
 
-    public static double distance(double x1, double y1, double x2, double y2){
-        return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) *  (x2 - x1));
+    public static double distance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
     }
 }
