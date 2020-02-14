c8e4f213f74e811cc186c20b86f9575654c25459
r.memetov <r.memetov@qiwi.com>
2020-02-14 15:37:15 +0300

    fix

--- sandbox/src/main/java/MyFirstProgramm.java
+++ sandbox/src/main/java/MyFirstProgramm.java
@@ -18,11 +18,7 @@ public class MyFirstProgramm {
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area(Square s){
-        return s.l * s.l;
-    }
 
-    public static double area(Rectangle r){
-        return r.a * r.b;
-    }
+
+
 }
--- sandbox/src/main/java/Square.java
+++ sandbox/src/main/java/Square.java
@@ -3,6 +3,11 @@ public class Square {
     public double l;
 
     public Square(double l){
+
         this.l = l;
+    }
+
+    public  double area(){
+        return this.l * this.l;
     }
 }
