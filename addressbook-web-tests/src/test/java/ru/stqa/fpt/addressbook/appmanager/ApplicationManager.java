cb1a1b3d0843c3afaea9ffe8853b01dc9b492d4f
r.memetov <r.memetov@qiwi.com>
2020-03-03 09:52:48 +0300

    Crossbrowsers testing

--- addressbook-web-tests/src/test/java/ru/stqa/fpt/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/fpt/addressbook/appmanager/ApplicationManager.java
@@ -5,20 +5,35 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-    protected WebDriver wd;
+     WebDriver wd;
 
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+
+        this.browser = browser;
+    }
 
     public void init() {
         System.setProperty("webdriver.chrome.driver", "utils/chromedriver.exe");
-        wd = new ChromeDriver();
+        if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.FIREFOX){
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.IE) {
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost:84/addressbook/");
         groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/ru/stqa/fpt/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/fpt/addressbook/tests/TestBase.java
@@ -1,12 +1,14 @@
 package ru.stqa.fpt.addressbook.tests;
 
+
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.fpt.addressbook.appmanager.ApplicationManager;
 
-public class TestBase extends ApplicationManager {
+public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
