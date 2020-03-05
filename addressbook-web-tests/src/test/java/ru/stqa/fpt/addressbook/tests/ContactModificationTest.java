f49a306652c04b1892893b60b13d7f9171902ce0
r.memetov <r.memetov@qiwi.com>
2020-03-05 13:36:49 +0300

    Выполнение предусловий для модификации

--- addressbook-web-tests/src/test/java/ru/stqa/fpt/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/fpt/addressbook/tests/ContactModificationTest.java
@@ -7,6 +7,11 @@ public class ContactModificationTest extends TestBase {
 
     @Test
     public void contactModification() {
+        if (! app.getContactHelper().isThereAContact())  {
+            app.getContactHelper().goToAddContactPage();
+            app.getContactHelper().createContact(new ContactData("Ivanov", "Ivan", "Moscow", "71234567890", "test@test.ru", "test2"), true);
+        }
+
         app.getContactHelper().editContact();
         app.getContactHelper().fillForm(new ContactData("Ivanov2", "Ivan2", "Omsk", "123", "email@test.com", null), false);
         app.getContactHelper().updateContact();
